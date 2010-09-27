655dfe50e2f1e709655e9b7c185758680bf7532d
Stevan Little <stevan@cpan.org>
2010-09-27 19:49:44 +0000

    Import of STEVAN/Forest-0.09 from CPAN.
    gitpan-cpan-distribution: Forest
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         STEVAN/Forest-0.09.tar.gz
    gitpan-cpan-author:       STEVAN
    gitpan-cpan-maturity:     released

--- lib/Forest/Tree/Builder/Callback.pm
+++ lib/Forest/Tree/Builder/Callback.pm
@@ -1,10 +1,10 @@
 package Forest::Tree::Builder::Callback;
 use Moose::Role;
 
-our $VERSION   = '0.08';
+our $VERSION   = '0.09';
 our $AUTHORITY = 'cpan:STEVAN';
 
-with 'Forest::Tree::Builder' => { excludes => [qw(create_new_subtree)] };
+with 'Forest::Tree::Builder' => { -excludes => [qw(create_new_subtree)] };
 
 has new_subtree_callback => (
     isa => "CodeRef|Str",
